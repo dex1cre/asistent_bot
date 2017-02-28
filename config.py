79bff12d7301716e9aa898d05128726227eda28e
dex1cre <dex1cre@mail.ru>
2017-02-28 18:29:14 +0300

    немного поработал с днями недели, но в итоге никаких толковых изменений не выдал

--- config.py
+++ config.py
@@ -1,1 +1,11 @@
 token = "362521239:AAE3wv5PLZ-BuntbSIIizAH40mLOYbfbfWo"
+days = ["понедельник","вторник","среда","четверг","пятница","суббота","воскресенье"]
+numbers = ["/0","/1","/2","/3","/4","/5","/6"]
+
+wt = False
+wt2 = False
+wt3 = False
+number_day = 0
+description = ""
+d_start = 0
+d_stop = 0
--- main.py
+++ main.py
@@ -65,14 +65,65 @@ def mn():
             bot.send_message(message.from_user.id, "До встречи!", reply_markup=hm)
 
         #Другие команды
+        #команда для просмотра задачи в данный момент времени
         @bot.message_handler(commands=["now"])
         def send_to_now(message):
             n = datetime.weekday(datetime.now())
             bot.send_message(message.from_user.id, n)
-
+        #команда для создания новых задач
+        @bot.message_handler(commands=["new"])
+        def send_to_new(message):
+            st = """
+Выберите день недели\n
+/0 - понедельник
+/1 - вторник
+/2 - среда
+/3 - четверг
+/4 - пятница
+/5 - суббота
+/6 - воскресенье
+"""
+            bot.send_message(message.from_user.id, st)
+        #команда для обработки дней недели
         @bot.message_handler(content_types=['text'])
         def repeat_all_messages(message):
-            bot.send_message(message.from_user.id, "hello")
+            if "/" in message.text:
+                if message.text in config.numbers:
+                    config.wt = True
+                    config.number_day = config.numbers.index(message.text)
+                    stt = config.days[config.number_day]
+                    bot.send_message(message.from_user.id, "Введите задание! на " + stt)
+                else:
+                    bot.send_message(message.from_user.id, "not command")
+            elif config.wt:
+                config.wt = False
+                config.wt2 = True
+                config.description = message.text
+                bot.send_message(message.from_user.id, "Введите время начала этого события:\nВведите час (пишется одной цифрой или числом)")
+            elif config.wt2:
+                config.wt2 = False
+                config.wt3 = True
+                print(int(message.text()))
+                try:
+                    config.d_start = int(message.text())
+                except:
+                    config.wt2 = True
+                    config.wt3 = False
+                    bot.send_message(message.from_user.id, "Не корректный ввод, введите заново")
+                else:
+                    bot.send_message(message.from_user.id, "Теперь введите время окончания")
+            elif config.wt3:
+                config.wt3 = False
+                try:
+                    config.d_stop = int(message.text())
+                except:
+                    config.wt2 = True
+                    config.wt3 = False
+                    bot.send_message(message.from_user.id, "Не корректный ввод, введите заново")
+                else:
+                    bot.send_message(message.from_user.id, "Ваше задание принято")
+
+
     except:
         print("Warning!")
 
